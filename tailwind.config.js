4df1b8da49ad3e2f7d5488c446217c8294f67494
Samuel Horn af Rantzien <samuel@samuelhorn.com>
2020-11-22 19:40:41 +0100

    turned of purging to get storybook working

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,7 +55,7 @@ module.exports = {
   plugins: [],
   purge: {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
+    enabled: false,
     content: [
       'components/**/*.vue',
       'pixelslices/**/*.vue',
